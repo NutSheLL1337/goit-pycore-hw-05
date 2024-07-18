1c971cb94a7989ea75c51b5e3e739330d9ce978f
NutSheLL <holnaz1337@gmail.com>
2024-07-18 18:50:48 +0300

    Changed name, fixed bugs v2.0

--- Task_1.py
+++ Task_1.py
@@ -1,8 +1,10 @@
-
+# Task 1
 
 def caching_fibonacci():
+    # Функція повертає внутрішню функцію fibonacci
     cache = {}
     def fibonacci(n):
+        # Обчислює n-те число Фібоначчі. Якщо число вже знаходиться у кеші, функція повертає значення з кешу
         if n <= 0:
             return 0
         elif n == 1:
@@ -12,7 +14,7 @@ def caching_fibonacci():
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
     
-    return fibonacci(n)
+    return fibonacci
     # КІНЕЦЬ ФУНКЦІЇ caching_fibonacci
 
 
@@ -23,3 +25,4 @@ fib = caching_fibonacci()
 # Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
 print(fib(10))  # Виведе 55
 print(fib(15))  # Виведе 610
+print(fib(20))  # Виведе 6765
