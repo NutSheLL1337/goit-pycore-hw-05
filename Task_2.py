065ed9ed1a836119c29e33897301cefc11abfae0
NutSheLL <holnaz1337@gmail.com>
2024-07-19 11:18:04 +0300

    Added regexp, for and sum, v2.0

--- Task_2.py
+++ Task_2.py
@@ -1,14 +1,19 @@
 from typing import Callable
 from decimal import Decimal
+import re
 
 
 def generator_numbers(text: str):
-    res = [Decimal(d.split("")) for d in text]
-    yield res
+    # Фукнція перевіряє в тексті наявність цілих чисел, перетворює кожен рядок в Decimal та повертає генератор
+    pattern = r'\d+\.\d+'
+    matches = re.findall(pattern, text)
+    for match in matches:
+        yield Decimal(match)
     
 
-def sum_profit(text: str, func: Callable):
-    return text + str(func)
+def sum_profit(text: str, func: Callable)-> Decimal:
+    # Підрахунок суми знайдених генератором цілих чисел з тексту
+    return sum(func(text))
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
